657e19278412f1913050805e5751516f20836f15
Aldo Palma <Disaster.2905@gmail.com>
2026-09-21 19:22:45 -0300

    Implementación inicial de ms-usuarios: Entidad JPA, Repositorio y Controller con conexión a MySQL

---
+++ ms-usuarios/src/main/java/com/example/ms_usuarios/controller/UsuarioController.java
@@ -0,0 +1,26 @@
+package com.example.ms_usuarios.controller;
+
+import com.example.ms_usuarios.model.Usuario;
+import com.example.ms_usuarios.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @GetMapping
+    public List<Usuario> obtenerTodos() {
+        return usuarioRepository.findAll();
+    }
+
+    @PostMapping
+    public Usuario crearUsuario(@RequestBody Usuario usuario) {
+        return usuarioRepository.save(usuario);
+    }
+}
